7c14d640302c5f6537d5e3c0e4f503407f204cbd
ra-lgt <raviajay9344@gmail.com>
2022-10-18 21:57:19 +0530

    Ajay

--- journey_begins/Home_Page/urls.py
+++ journey_begins/Home_Page/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('',views.home)
+    path('',views.home),
+    path('login',views.login),
 ]
--- journey_begins/Home_Page/views.py
+++ journey_begins/Home_Page/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .forms import Register
 # Create your views here.
 def home(request):
-    return render(request,"./Home_Page/home.html")
+    if(request.method=='POST'):
+        form=Register(request.POST)
+
+        if(form.is_valid()):
+            email=form.cleaned_data['email_id']
+            return HttpResponse(email)
+
+    else:
+        return render(request,'home.html')
+def login(request):
+    return HttpResponse("hiii")
+
+
+
