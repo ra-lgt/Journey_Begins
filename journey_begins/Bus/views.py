b86d26be7e92518f9869ddd082f101dfd42580b7
ra-lgt <raviajay9344@gmail.com>
2022-10-20 22:42:27 +0530

    Ajay

--- journey_begins/Bus/views.py
+++ journey_begins/Bus/views.py
@@ -1,39 +1,42 @@
 from django.shortcuts import render
 from .bus_data import Data_set
 import time
+from datetime import date as dates
 import random
 # Create your views here.
 bus_list=Data_set().data
 curr_time=time.gmtime()
-hour=random.randint(1,5)
+
 
 def search_bus(request):
-    global bus_list
-    global curr_time
-    global hour
-
-
+    global hours
     start_time=0
     if(request.method=="POST"):
         depature=request.POST.get('depature')
         arrive=request.POST.get('arrive')
         date=request.POST.get('date')
 
+        input_date=date.split("-")
+        a=str(dates.today())
+        curr_date=a.split("-")
+
+        if(input_date[0]<curr_date[0]):
+            return render(request,'404.html')
+        else:
+            if(input_date[1]<curr_date[1]):
+                return render(request, '404.html')
+            elif(input_date[2]<curr_date[2]):
+                return render(request, '404.html')
+
+
         try:
             fee=[]
             name=[]
-            start_time=curr_time.tm_hour+hour
-            if(start_time>24):
-                start_time-curr_time.tm_hour
-
-            end_time=start_time+hour
-            if(end_time>24):
-                end_time-curr_time.tm_hour
-
             start=[]
             end=[]
             offers=[]
-            mins=[]
+
+            curr_hour=curr_time.tm_hour
 
 
             if(bus_list[depature][arrive]):
@@ -45,28 +48,25 @@ def search_bus(request):
                     else:
                         offers.append(temp[i] - random.choice([20, 30, 15, 9, 2, 6, 8, 16,20,11.5]))
                         fee.append(temp[i])
-                for j in range(len(name)):
-                    if (start_time < 0 or start_time==24):
-                        start_time = 0
-                    elif (start_time > 24):
-                        start_time=int(curr_time.tm_hour-start_time)
-                    if (end_time < 0 or end_time==24):
-                        end_time = 0
-                    elif (end_time > 24):
-                        end_time=int(curr_time.tm_hour-end_time)
-                    mins.append(random.randint(1,60))
+                for i in range(len(name)):
+                    start_time=curr_hour+random.randint(1,5)
+                    if(start_time>24):
+                        start_time=abs(start_time-24)
+                    start.append((start_time))
 
-                    start.append(abs(start_time))
-                    end.append(abs(end_time))
-                    start_time+=random.choice([1,2,3])
-                    end_time+=random.choice([1,2,3])
+                for j in start:
+                    end_time=j+random.randint(1,5)
+                    if(end_time>24):
+                        end_time=abs(end_time-24)
+                    end.append(end_time)
+                print(end)
+                print(start)
 
-                print(fee,name,start,end,offers,(mins))
                 return render(request,'searchbus.html',{
                     'depature':depature,
                     'arrive':arrive,
                     'date':date,
-                    'datas':zip(name,fee,start,end,offers,mins)})
+                    'datas':zip(name,fee,start,end,offers)})
         except:
             return render(request,'404.html')
     else:
