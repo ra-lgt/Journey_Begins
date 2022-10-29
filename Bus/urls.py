61835b4c963986b28347d4090c24040147b841b5
H-Gowshick <107055849+H-Gowshick@users.noreply.github.com>
2022-10-29 09:24:20 +0530

    Add files via upload

---
+++ Bus/urls.py
@@ -0,0 +1,13 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('',views.search_bus),
+    path('bus_filter_Ac',views.bus_filter_Ac),
+    path('bus_filter_Nc',views.bus_filter_Nc),
+    path('bookseats/<starting>',views.bookseats),
+    path('payment_success',views.payment_success),
+    #path('passenger',views.passenger),
+
+
+    ]
